6adc7ad72a27793e124498c088c03e9e1d3ea6f5
Tamara <tamara.czakert@students.fh-hagenberg.at>
2017-05-27 11:08:01 +0200

    change username on players site

--- src/routes/Players.jsx
+++ src/routes/Players.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { RadioButton, RadioButtonGroup } from 'material-ui/RadioButton';
 import Paper from 'material-ui/Paper';
+import TextField from 'material-ui/TextField';
+
 import users from '../users';
 
 
@@ -16,10 +18,27 @@ const styles = {
 
 
 export default class Players extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: 'testuser',
+    };
+  }
+
+  handleNameChange(e) {
+    this.setState({ username: e.target.value });
+  }
+
   render() {
     return <div>
       <h1>{this.constructor.name}</h1>
       <Paper style={styles.container}>
+        <TextField
+            hintText={this.state.username}
+            floatingLabelText="Change your Username"
+            onChange={ this.handleNameChange }
+        /><br />
         <RadioButtonGroup name="users"
                           onChange={(e, value) => this.setStation(value)}>
           {users.map(user => <RadioButton value={user.name}
